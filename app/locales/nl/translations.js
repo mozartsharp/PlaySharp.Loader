6a507f43800ae4cb113eabb19f0ee1d1edbc0620
LILITHLOVEGOOD <lorrah123@gmail.com>
2016-01-22 20:34:09 +0100

    Update translations.js

--- app/locales/nl/translations.js
+++ app/locales/nl/translations.js
@@ -16,12 +16,12 @@ const result = {
     "sort_by": {
       "last_update": "Meest recente update",
       "name": "Naam",
-      "rating": "Ranking"
+      "rating": "Score"
     },
     "written_by": "Deze assembly is geschreven door {author} en de meest recente update was op {until-now}"
   },
   "layout": {
-    "logout": "Uitloggen",
+    "logout": "Afmelden",
     "navigation": {
       "applog": "Applog",
       "assemblies": "Assemblies",
@@ -40,7 +40,7 @@ const result = {
         "title": "Mijn groepen"
       },
       "entry": {
-        "author": "Auteur van de assembly",
+        "author": "Schrijver van de assembly",
         "name": "Naam van de assembly",
         "version": "Versie van de assembly"
       }
