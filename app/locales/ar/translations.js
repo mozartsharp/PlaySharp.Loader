cc9110853f08e4f9f8dcfd3a7a4844c47e71918e
jQuery <nathan@zavox.nl>
2016-01-22 21:36:10 +0100

    Added Arabic

---
+++ app/locales/ar/translations.js
@@ -0,0 +1,53 @@
+import Ember from 'ember';
+import EN from './champions';
+
+const result = {
+  "assemblies": {
+    "buttons": {
+      "install": "تثبيت",
+      "options": "خيارات"
+    },
+    "search_by": {
+      "assemblies": "الشخصية",
+      "libraries": "المكاتب",
+      "utilities": "المساعد"
+    },
+    "search_phrase": "ابحث عن اسمبليس",
+    "sort_by": {
+      "last_update": "اخر تحديث",
+      "name": "الاسم",
+      "rating": "التقدير"
+    },
+    "written_by": " هذا الاسمبلي كتب بواسطة {author} واخر تحديث كان {until-now}"
+  },
+  "layout": {
+    "logout": "الخروج",
+    "navigation": {
+      "applog": "سجل التطبيق",
+      "assemblies": "الاسملبيس",
+      "changelog": "سجل التغيير",
+      "news": "الاخبار",
+      "profiles": "الملفات الشخصية",
+      "settings": "الاعدادات"
+    },
+    "welcome_back": "مرحبا بعودتك, {username}!"
+  },
+  "profiles": {
+    "set": {
+      "root": {
+        "create": "انتج مجموعة جديدة",
+        "default": "الملف الافتراضي {index}",
+        "title": "مجموعاتي"
+      },
+      "entry": {
+        "author": "مؤلف الاسمبلي",
+        "name": "اسم الاسمبلي",
+        "version": "نسخة الاسمبلي"
+      }
+    }
+  }
+};
+
+Ember.merge(result, EN);
+
+export default result;
